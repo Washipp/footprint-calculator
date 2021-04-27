9cec7de34fa757f4e9b2181f3ab4beaff85058dc
Silas Meier <silas.meier@bluewin.ch>
2021-04-27 09:09:56 +0200

    Added own favicon.ico from freefavicon.com. Added the helper function to more easily calculate the kWh used. Updated the input fields. Added a new interface that contains the constants.

--- src/app/calculator.service.ts
+++ src/app/calculator.service.ts
@@ -2,20 +2,40 @@ import {Injectable} from '@angular/core';
 
 export interface CalculatorData {
   timeOnZoom: number;
-  usingWebcam: boolean;
-  timeOnZoomWithWebcam: number;
+  usingWebcam?: boolean;
+  timeOnZoomWithWebcam?: number;
+  screenShareTime: number;
+  webcamTime: number;
+  listeningTime: number;
   // TODO: add the necessary fields
 }
-
+interface Constants {
+  electricityIntensity: number; // in kWh/GB
+  voip: number; // in kbps
+  webcam: number; // in kbps
+  screenShare: number; // in kbps
+}
 @Injectable({
   providedIn: 'root'
 })
 export class CalculatorService {
 
-  constructor() { }
+  c: Constants;
+
+  constructor() {
+    this.c = {
+      electricityIntensity: 0.015,
+      voip: 60,
+      webcam: 600,
+      screenShare: 50,
+    };
+  }
 
   createEmptyData(): CalculatorData {
     return {
+      listeningTime: undefined,
+      screenShareTime: undefined,
+      webcamTime: undefined,
       timeOnZoom: undefined,
       usingWebcam: false,
       timeOnZoomWithWebcam: undefined
@@ -24,8 +44,18 @@ export class CalculatorService {
   }
 
   calculateFootprint(data: CalculatorData): number {
-    return Math.random() * (data.timeOnZoom - data.timeOnZoomWithWebcam + 1) + data.timeOnZoomWithWebcam;
+    // return Math.random() * (data.timeOnZoom - data.timeOnZoomWithWebcam + 1) + data.timeOnZoomWithWebcam;
     // TODO: insert formula to calculate the footprint
-    // return 0;
+    return this.calculateKWh(this.c.screenShare) * data.screenShareTime
+         + this.calculateKWh(this.c.webcam) * data.webcamTime
+         + this.calculateKWh(this.c.voip) * data.listeningTime;
+  }
+
+  /**
+   * Helper function that calculates how much kWh (kilowatt hours) per GB are used during 1 hour.
+   * @param bandwidth Provided bandwidth in kbps.
+   */
+  calculateKWh(bandwidth: number): number {
+    return (bandwidth / (8 * 1000 * 100)) * 60 * 60 * this.c.electricityIntensity;
   }
 }
